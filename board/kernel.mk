2459332edf2495d10655183f6da46c4efe1f6162
Shekhawat2 <anandsingh215@yahoo.com>
2017-12-17 17:56:00 +0000

    Use linaro 7 toolchain for kernel
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,3 +2,5 @@
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_CONFIG := lineageos_tomato_defconfig
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/linaro7/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linaro-linux-android-
