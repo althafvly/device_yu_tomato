a126572aaadccc685d1fa06809997f50b20e4088
althafvly <althafvly@gmail.com>
2018-02-08 16:51:34 +0000

    density changes

--- init/init_tomato.cpp
+++ init/init_tomato.cpp
@@ -40,7 +40,7 @@
 using android::base::GetProperty;
 using android::init::property_set;
 using android::init::import_kernel_cmdline;
-static int display_density = 230;
+static int display_density = 320;
 
 static void import_cmdline(const std::string& key,
         const std::string& value, bool for_emulator __attribute__((unused)))
@@ -48,7 +48,7 @@ static void import_cmdline(const std::string& key,
     if (key.empty()) return;
 
     if (key == "panel.xres" && value == "1080") {
-        display_density = 410;
+        display_density = 480;
     }
 }
 
@@ -58,7 +58,7 @@ void init_target_properties()
     import_kernel_cmdline(0, import_cmdline);
     snprintf(density, sizeof(density), "%d", display_density);
     property_set("ro.sf.lcd_density", density);
-    if (display_density == 410) {
+    if (display_density == 480) {
         property_set("ro.product.model", "YU5510");
         property_set("dalvik.vm.heapstartsize", "16m");
         property_set("dalvik.vm.heapgrowthlimit", "192m");
