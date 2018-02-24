0639375462cef621a4ee04adbb7c8fc683a7694b
althafvly <althafvly@gmail.com>
2018-02-24 17:06:32 +0000

    Build Telephony

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -15,3 +15,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
